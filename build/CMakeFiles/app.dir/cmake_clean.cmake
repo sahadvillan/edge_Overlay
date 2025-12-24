0a8a207072ace21f766c80527f1cdbf24918da8e
Sahad Muhammed Villan <143242622+sahadvillan@users.noreply.github.com>
2025-12-24 13:30:02 +0100

    CMAKe update

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/cpu_pipeline.cpp.obj"
+  "CMakeFiles/app.dir/src/cpu_pipeline.cpp.obj.d"
+  "CMakeFiles/app.dir/src/main.cpp.obj"
+  "CMakeFiles/app.dir/src/main.cpp.obj.d"
+  "app.exe"
+  "app.exe.manifest"
+  "app.pdb"
+  "libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
